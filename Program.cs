5399414aefc96e708704d088be5092d9b9426bc0
Ike Ton <iketon@gmail.com>
2012-08-06 19:31:14 -0500

    Added a bullet to interact with.
    Added a bullet that moves across the screen. And set up work so we may
    begin interacting with it. This is the bases for adding AI's laters.

--- Program.cs
+++ Program.cs
@@ -10,11 +10,15 @@ namespace Lesson2
         // wheither or not exit
         static bool s_ExitGame = false;
 
-        // screen coord
+        // Screen coord. Here we store where we want the player to be.
         // [0,0] [0,1] [0,2] [0,3] ...
         // [1,0] [1,1] [1,2] [1,3] ...
         static int s_PlayerXPos = 20;
         static int s_PlayerYPos = 10;
+
+        // Screen coord. Here we are going to want to store the position of the bullet.
+        static int s_bulletXPos = 40;
+        static int s_bulletYPos = 3;
 
         static void Main(string[] args)
         {
@@ -72,6 +76,21 @@ namespace Lesson2
                 if (s_PlayerYPos > 20)
                     s_PlayerYPos = 20;
             }
+
+            // now we are going to update the bullets movement
+            s_bulletXPos -= 1;
+
+            // if the bullet hits the end we want it to wrap around
+            if (s_bulletXPos < 0)
+                s_bulletXPos = 40;
+
+            // now we can check if the bullet hit the player
+            if (s_bulletXPos == s_PlayerXPos
+                && s_bulletYPos == s_PlayerYPos)
+            { 
+                // there is a hit if they occupy the same space!
+                // TODO: we will handle this in the next exercise.
+            }
         }
 
         static void DrawGraphics()
@@ -84,6 +103,10 @@ namespace Lesson2
             
             // Draw rhe player
             Console.Write("O");
+
+            // now we need to draw the bullet
+            Console.SetCursorPosition(s_bulletXPos, s_bulletYPos);
+            Console.Write(".");
         }
     }
 }
--- readme.txt
+++ readme.txt
@@ -5,3 +5,5 @@ The main purpose of this lesson is to create a simple game loop, and create an a
 - Check in #1. We create a shell by create a new c# console app.
 
 - Check in #2. Create a game loop. Take in user input and draw the player "O".
+
+- Check in #3. Create a simple bullet, that moves across the screen. Set work so that we may interact with it.
